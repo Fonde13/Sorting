e9b397eb863258652321a0d5274141e8ba3c229d
FONDE <ziz8fhad@gmail.com>
2023-09-24 20:32:14 +0300

    Added SelectionSort in main

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,6 +1,18 @@
 public class Sorter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr1 = {9,14,3,2,43,11,58,22};
+        System.out.println("Before Selection Sort");
+        for(int i:arr1){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        selectionSort(arr1);//sorting array using selection sort
+
+        System.out.println("After Selection Sort");
+        for(int i:arr1){
+            System.out.print(i+" ");
+        }
 
     }
     public static void selectionSort(int[] arr){
